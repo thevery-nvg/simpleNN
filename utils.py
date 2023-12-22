15734afe17f28cd86cecd426163c26d9809a25ce
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2023-12-22 13:21:13 +0400

    Update utils.py
    defined activation functions : relu sigmoid

--- utils.py
+++ utils.py
@@ -26,3 +26,10 @@ def save_model(weights_input_to_hidden, weights_hidden_to_output):
 def load_model():
     with np.load('model.npz') as f:
         return f['weights_input_to_hidden'], f['weights_hidden_to_output']
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
