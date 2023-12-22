b8cc2e123ac60e0af5b61b97d65fc01a8d485e35
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2023-12-22 13:24:25 +0400

    Update utils.py
    softmax and tanh added

--- utils.py
+++ utils.py
@@ -33,3 +33,10 @@ def relu(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    exps = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return exps / np.sum(exps, axis=-1, keepdims=True)
+
+def tanh(x):
+    return np.tanh(x)
