7f6aec2a22004cf05e32fae955906de1c433d37e
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2024-04-27 10:48:10 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,30 +2,27 @@ import numpy as np
 
 
 def load_dataset():
-    with np.load('mnist.npz') as f:
+    with np.load('new_data.npz') as f:
         # convert from rgb to unit rgb (0-1)
         images = f['x_train'].astype('float32') / 255
 
         # reshape from (60000,28,28) into (60000,784)
-        images = np.reshape(images, (60000, 784))
-
-        # labels
+        images = np.reshape(images, (images.shape[0], 784))
+        
         labels = f['y_train']
-
-        # convert to output layer format
         labels = np.eye(10)[labels]
-
         return images, labels
 
 
-def save_model(weights_input_to_hidden, weights_hidden_to_output):
-    np.savez('model.npz', weights_hidden_to_output=weights_hidden_to_output,
-             weights_input_to_hidden=weights_input_to_hidden)
+def save_model(weights_input_to_hidden, weights_hidden_1_to_hidden_2, weights_hidden_2_to_output):
+    np.savez('model.npz', weights_input_to_hidden=weights_input_to_hidden,
+             weights_hidden_1_to_hidden_2=weights_hidden_1_to_hidden_2,
+             weights_hidden_2_to_output=weights_hidden_2_to_output)
 
 
 def load_model():
     with np.load('model.npz') as f:
-        return f['weights_input_to_hidden'], f['weights_hidden_to_output']
+        return f['weights_input_to_hidden'], f['weights_hidden_1_to_hidden_2'], f['weights_hidden_2_to_output']
 
 
 def relu(x):
