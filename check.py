edec3981519013e082891b2936fe16eee7fde025
thevery-nvg <129183313+thevery-nvg@users.noreply.github.com>
2024-04-27 10:48:22 +0400

    Update check.py

--- check.py
+++ check.py
@@ -2,19 +2,27 @@ import random, utils
 import numpy as np
 import matplotlib.pyplot as plt
 
-# CHECK
-images, labels = utils.load_dataset()
-weights_input_to_hidden, weights_hidden_to_output = utils.load_model()
 
-bias_input_to_hidden = np.zeros((30, 1))
-bias_hidden_to_output = np.zeros((10, 1))
+images, labels = utils.load_dataset()
+weights_input_to_hidden, weights_hidden_1_to_hidden_2, weights_hidden_2_to_output = utils.load_model()
+
+bias_input_to_hidden = np.zeros((50, 1))  
+bias_hidden_1_to_hidden_2 = np.zeros((50, 1))  
+bias_hidden_to_output = np.zeros((10, 1))  
+
 
 test_image = random.choice(images)
-# Predict
+
+
 image = np.reshape(test_image, (-1, 1))
-hidden_raw = bias_input_to_hidden + weights_input_to_hidden @ image
-hidden = 1 / (1 + np.exp(-hidden_raw))
-output_raw = bias_hidden_to_output + weights_hidden_to_output @ hidden
+
+hidden_1_raw = bias_input_to_hidden + weights_input_to_hidden @ image
+hidden_1 = 1 / (1 + np.exp(-hidden_1_raw))
+
+hidden_2_raw = bias_hidden_1_to_hidden_2 + weights_hidden_1_to_hidden_2 @ hidden_1
+hidden_2 = 1 / (1 + np.exp(-hidden_2_raw))
+
+output_raw = bias_hidden_to_output + weights_hidden_2_to_output @ hidden_2
 output = 1 / (1 + np.exp(-output_raw))
 
 plt.imshow(test_image.reshape(28, 28), cmap='Greys')
